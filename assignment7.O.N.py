50640494a2bc4dd27661f8c666b9e39d23361254
Ivan007b <117934395+Ivan007b@users.noreply.github.com>
2023-01-19 18:41:06 +0200

    Update assignment7.O.N.py

--- assignment7.O.N.py
+++ assignment7.O.N.py
@@ -1,1 +1,39 @@
-# Here we write task123
+def my_function(age = "12"):
+    print("How old are you? " + age)
+my_function("11")
+my_function("12")
+my_function("13")
+
+a = [1,2,3]
+
+
+def my_function(list):
+    return sum(list)
+print(my_function(a))
+
+    def my_function(c):
+    c = input("Write str: ")
+if my_function(c):    
+    c.reverse()
+print("I reverse your str",  my_function(c))
+
+def my_function(num):
+    num = int(input("Enter  any number to  test whether it is odd or even: "))
+    if (num % 2) == 0:
+        print("The   number is even", my_function(num))
+    else:
+        print("The provided number is odd", my_function(num))
+
+def my_function(a):
+    a =(input("Enter  any str or number to  test whether it is str or int: "))
+if my_function(str(a)):
+        print("The str")
+else:
+
+        print("The int")
+
+def func (a):
+    a = input(" write smth:")
+    if type == 'some type':
+        print('massage')
+
